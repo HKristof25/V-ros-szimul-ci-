bc2dcfc17702351bfa0441761601cf76fe31199a
Cornuta.David <cornutadavid0@gmail.com>
2025-03-24 21:39:35 +0100

    Reszletek reszletezese, elv grafikus felulet kesz

--- Állomváros/Állomváros/Reszlet.xaml.cs
+++ Állomváros/Állomváros/Reszlet.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -35,35 +36,91 @@ namespace Állomváros
         {
             ContentPanel.Children.Clear();
 
-            if(actionType == "torles")
+            if (actionType == "torles")
             {
-                ContentPanel.Children.Add(new TextBlock { Text = "Típus", FontSize = 28, Foreground = Brushes.White });
-                ContentPanel.Children.Add(new TextBox { Name = "Típus", FontSize = 28 });
-                ContentPanel.Children.Add(new TextBlock { Text = "Befolyás az elégedettségen", FontSize = 28, Foreground = Brushes.White });
-                
+                ContentPanel.Children.Add(new Label { Content = "Azonosító", Foreground = Brushes.White, FontSize = 20 });
+                ContentPanel.Children.Add(new TextBox { Text = "Azonosító", FontSize = 20, Foreground = Brushes.White });
+                ContentPanel.Children.Add(new Label { Content = "Név", Foreground = Brushes.White, FontSize = 20 });
+                ContentPanel.Children.Add(new TextBox { Text = "Név",  Foreground = Brushes.White, FontSize = 20});
+                ContentPanel.Children.Add(new Label { Content = "Típus", Foreground = Brushes.White, FontSize = 20 });
+                TextBox tipus = new TextBox { Name = "tipus", Text = "Típus", FontSize = 20, Foreground = Brushes.White };
+                ContentPanel.Children.Add(tipus);
+                ContentPanel2.Children.Add(new Label { Content = "Kezdő dátum", Foreground = Brushes.White, FontSize = 20 });
+                ContentPanel2.Children.Add(new TextBox { Text = "Kezdő dátum", FontSize = 20, Foreground = Brushes.White });
+                ContentPanel2.Children.Add(new Label { Content = "Befejező dátum", Foreground = Brushes.White, FontSize = 20 });
+                ContentPanel2.Children.Add(new TextBox { Text = "Befejező dátum", FontSize = 20, Foreground = Brushes.White });
+                ContentPanel2.Children.Add(new Label { Content = "Költség", Foreground = Brushes.White, FontSize = 20 });
+                ContentPanel2.Children.Add(new TextBox { Text = "Költség", FontSize = 20, Foreground = Brushes.White });
+                ContentPanel2.Children.Add(new Label { Content = "Befolyás az elégedettségen", FontSize = 20, Foreground = Brushes.White });
+                if (tipus.Text.ToLower() == "lakóház")
+                {
+                    ContentPanel2.Children.Add(new Label { Content = "Befolyás a népességen", Foreground = Brushes.White, FontSize = 20});
+                }
             }
+
 
             if (actionType == "epulet")
             {
-                ContentPanel.Children.Add(new TextBlock { Foreground = Brushes.White, FontSize = 28, Text = "Méret"});
-                ContentPanel.Children.Add(new TextBox { Name = "meret" });
-                ContentPanel.Children.Add(new TextBlock { Text = "Befolyás a lakosságon", FontSize = 28,Foreground = Brushes.White });
-                ContentPanel.Children.Add(new TextBlock { Text = "Ár", FontSize = 28, Foreground = Brushes.White });
-                ContentPanel.Children.Add(new TextBlock { Text = "Idő", FontSize = 28, Foreground = Brushes.White });
+                ContentPanel.Children.Add(new Label { Content = "Azonosító", Foreground = Brushes.White, FontSize = 20 });
+                ContentPanel.Children.Add(new TextBox { Text = "Azonosító", Foreground = Brushes.White, FontSize = 20 });
+                ContentPanel.Children.Add(new Label { Content = "Név", Foreground = Brushes.White, FontSize = 20 });
+                ContentPanel.Children.Add(new TextBox { Text = "Név", Foreground = Brushes.White, FontSize = 20 });
+                ContentPanel.Children.Add(new Label { Content = "Típus", Foreground = Brushes.White, FontSize = 20 });
+                TextBox tipus = new TextBox { Name = "tipus", Text = "Típus", FontSize = 20, Foreground = Brushes.White };
+                ContentPanel.Children.Add(tipus);
+                ContentPanel.Children.Add(new Label { Content = "Méret", Foreground = Brushes.White, FontSize = 20 });
+                ContentPanel.Children.Add(new TextBox { Text = "Méret", Foreground = Brushes.White, FontSize = 20 });
+                ContentPanel2.Children.Add(new Label { Content = "Költség", FontSize = 20, Foreground = Brushes.White });
+                ContentPanel2.Children.Add(new TextBox { Text = "Költség", FontSize = 20, Foreground = Brushes.White });
+                ContentPanel2.Children.Add(new Label { Content = "Kezdő dátum", FontSize = 20, Foreground = Brushes.White });
+                ContentPanel2.Children.Add(new TextBox{ Text = "Kezdő dátum", FontSize = 20, Foreground = Brushes.White });
+                ContentPanel2.Children.Add(new Label { Content = "Befejező dátum", FontSize = 20, Foreground = Brushes.White });
+                ContentPanel2.Children.Add(new TextBox { Text = "Befejező dátum", FontSize = 20, Foreground = Brushes.White });
+                ContentPanel2.Children.Add(new Label { Content = "Befolyás az elégedettségen", FontSize = 20, Foreground = Brushes.White });
+                if (tipus.Text.ToLower() == "lakóház")
+                {
+                    ContentPanel2.Children.Add(new Label { Content = "Befolyás a népességen", FontSize = 20, Foreground = Brushes.White });
+                }
+
             }
+
             if (actionType == "szolgaltatas")
             {
-                ContentPanel.Children.Add(new TextBlock { Text = "Típus", FontSize = 28, Foreground = Brushes.White });
-                ContentPanel.Children.Add(new TextBox { Name = "Típus", FontSize = 28 });
-                ContentPanel.Children.Add(new TextBlock { Text = "Befolyás az elégedettségen", FontSize = 28, Foreground = Brushes.White });
-                ContentPanel.Children.Add(new TextBlock { Text = "Ár", FontSize = 28, Foreground = Brushes.White });
-                ContentPanel.Children.Add(new TextBlock { Text = "Idő", FontSize = 28, Foreground = Brushes.White });
+                ContentPanel.Children.Add(new Label { Content = "Azonosító", Foreground = Brushes.White, FontSize = 20});
+                ContentPanel.Children.Add(new TextBox { Text = "Azonosító", Foreground = Brushes.White, FontSize = 20 });
+                ContentPanel.Children.Add(new Label { Content = "Név", Foreground = Brushes.White, FontSize = 20 });
+                ContentPanel.Children.Add(new TextBox { Text = "Név", FontSize = 20, Foreground = Brushes.White });
+                ContentPanel.Children.Add(new Label { Content = "Típus", FontSize = 20, Foreground = Brushes.White });
+                ContentPanel.Children.Add(new TextBox { Text = "Típus", FontSize = 20, Foreground = Brushes.White });
+                ContentPanel.Children.Add(new Label { Content = "Havi költésg", Foreground = Brushes.White, FontSize = 20 });
+                ContentPanel.Children.Add(new TextBox { Text = "Havi költés", Foreground = Brushes.White, FontSize = 20 });
+                ContentPanel2.Children.Add(new Label { Content = "Költség", FontSize = 20, Foreground = Brushes.White });
+                ContentPanel2.Children.Add(new TextBox { Text = "Költség", FontSize = 20, Foreground = Brushes.White });
+                ContentPanel2.Children.Add(new Label { Content = "Kezdő dátum", FontSize = 20, Foreground = Brushes.White });
+                ContentPanel2.Children.Add(new TextBox { Text = "Kezdő dátum", FontSize = 20, Foreground = Brushes.White });
+                ContentPanel2.Children.Add(new Label { Content = "Befejező dátum", FontSize = 20, Foreground = Brushes.White });
+                ContentPanel2.Children.Add(new TextBox { Text = "Befejező dátum", FontSize = 20, Foreground = Brushes.White });
+                ContentPanel2.Children.Add(new Label { Content = "Befolyás az elégedettségen", FontSize = 20, Foreground = Brushes.White });
             }
+
             if(actionType =="karbantartas")
             {
-                ContentPanel.Children.Add(new TextBlock { Text = "Ár", FontSize = 28, Foreground = Brushes.White });
-                ContentPanel.Children.Add(new TextBlock { Text = "Befolyás az elégedettségen", FontSize = 28, Foreground = Brushes.White });
-                ContentPanel.Children.Add(new TextBlock { Text = "Idő", FontSize = 28, Foreground = Brushes.White });
+                ContentPanel.Children.Add(new Label { Content = "Azonosító", Foreground = Brushes.White, FontSize = 20 });
+                ContentPanel.Children.Add(new TextBox { Text = "Azonosító", Foreground = Brushes.White, FontSize = 20 });
+                ContentPanel.Children.Add(new Label { Content = "Név", Foreground = Brushes.White, FontSize = 20 });
+                ContentPanel.Children.Add(new TextBox { Text = "Név", FontSize = 20, Foreground = Brushes.White });
+                ContentPanel.Children.Add(new Label { Content = "Havi költésg", Foreground = Brushes.White, FontSize = 20 });
+                ContentPanel.Children.Add(new TextBox { Text = "Havi költés", Foreground = Brushes.White, FontSize = 20 });
+                ContentPanel.Children.Add(new Label { Content = "Költség", FontSize = 20, Foreground = Brushes.White });
+                ContentPanel.Children.Add(new TextBox { Text = "Költség", FontSize = 20, Foreground = Brushes.White });
+                ContentPanel2.Children.Add(new Label { Content = "Kezdő dátum", FontSize = 20, Foreground = Brushes.White });
+                ContentPanel2.Children.Add(new TextBox { Text = "Kezdő dátum", FontSize = 20, Foreground = Brushes.White });
+                ContentPanel2.Children.Add(new Label { Content = "Befejező dátum", FontSize = 20, Foreground = Brushes.White });
+                ContentPanel2.Children.Add(new TextBox { Text = "Befejező dátum", FontSize = 20, Foreground = Brushes.White });
+                ContentPanel2.Children.Add(new Label { Content = "Érintett épületek", Foreground = Brushes.White, FontSize = 20 });
+                ContentPanel2.Children.Add(new TextBox { Text = "Érintett épületek", FontSize = 20, Foreground = Brushes.White });
+                ContentPanel2.Children.Add(new Label { Content = "Befolyás az elégedettségen", FontSize = 20, Foreground = Brushes.White });
+                
             }
         }
     }
